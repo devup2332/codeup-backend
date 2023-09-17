2068a678f6aa9f7a7912d839f19c82c7d980f8fe
Diego Rojas <devup2332@gmail.com>
2023-09-17 01:31:52 -0500

    master: Edited redirect URL

--- src/utils/passport/googlestrategy.ts
+++ src/utils/passport/googlestrategy.ts
@@ -8,7 +8,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, "google") {
 		super({
 			clientID: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: "http://localhost:8000/auth/google/redirect",
+			callbackURL: `${process.env.CLIENT_URL}/auth/google/redirect`,
 			scope: ["email", "profile"],
 		});
 	}
