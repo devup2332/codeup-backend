c199de741b34d2085e527d2ed98a33f0daf05010
Diego Rojas <devup2332@gmail.com>
2023-09-17 01:41:07 -0500

    master: Edited host

--- src/utils/passport/googlestrategy.ts
+++ src/utils/passport/googlestrategy.ts
@@ -8,7 +8,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, "google") {
 		super({
 			clientID: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: "/auth/google/redirect",
+			callbackURL: `${process.env.HOST}/auth/google/redirect`,
 			scope: ["email", "profile"],
 		});
 	}
