c271ad8f778805f5688f797c5529876f1a4aa55a
Diego Rojas <devup2332@gmail.com>
2023-10-24 23:24:06 -0500

    master: Edited cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { CLIENT_URL, SERVER_PORT } from "./environments/environments";
+import { SERVER_PORT } from "./environments/environments";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors({
-		origin: CLIENT_URL,
+		origin: "*",
 	});
 	await app.listen(SERVER_PORT || 8000);
 }
