a07ac870a2684924e0e7f887204b6e939531c179
Diego Rojas <devup2332@gmail.com>
2023-10-26 02:15:34 -0500

    master: Edited env variables

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import { AppModule } from "./app.module";
 import environments from "./environments/environments";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	await app.listen(environments.others.PORT || 3000, "0.0.0.0");
+	const app = await NestFactory.create(AppModule, {
+		cors: {
+			origin: environments.others.CLIENT_URL,
+		},
+		logger: ["warn", "error", "debug"],
+	});
+	await app.listen(environments.others.PORT || 8000, "0.0.0.0", () =>
+		console.log(
+			`SERVER LISTENING ON PORT ${environments.others.PORT} IN ${process.env.NODE_ENV} MODE`
+		)
+	);
 }
-console.log({ environments });
 bootstrap();
