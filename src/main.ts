912b0f24841ead32eb70f11afb57be5e39d976da
diego rojas <devup2332@gmail.com>
2023-10-25 23:35:59 -0500

    master: M

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {
 		logger: ["error", "warn", "log", "debug"],
 		cors: {
-			origin: environments.others.CLIENT_URL,
+			origin: "https://codeup-alpha.vercel.app",
 		},
 	});
-	await app.listen(environments.others.SERVER_PORT || 8000);
+	await app.listen( 8000);
 }
 console.log({ environments });
 bootstrap();
