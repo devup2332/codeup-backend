3e61fa913e1f215e8da7def7e3ba4beb37f54144
diego rojas <devup2332@gmail.com>
2023-10-25 23:22:16 -0500

    master: Edited

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import environments from "./environments/environments";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {
+		logger: ["error", "warn", "log", "debug"],
 		cors: {
 			origin: environments.others.CLIENT_URL,
 		},
